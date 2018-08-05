641deb20d8f68d3996cfc1479c20728d14e5ac6f
TomoeFox <battleloren@gmail.com>
2018-08-05 14:54:04 +0300

    Add logging function for request/response

--- main.cpp
+++ main.cpp
@@ -1,37 +1,59 @@
 #include <iostream>
 #include <string>
 #include <cpprest/ws_client.h>
+#include <sys/stat.h>
+#include <fstream>
+#include <chrono>
+#include <time.h>
 
 using namespace std;
 using namespace web;
 using namespace web::websockets::client;
 
-string Message(string a) {
-string response;
-  websocket_client client;
-  client.connect("ws://echo.websocket.org").wait();
-
-  websocket_outgoing_message out_msg;
+ofstream _log("./logs/log",ios::app);
+time_t current = time(0);
+string _datatime = ctime(&current);
 
 
-  out_msg.set_utf8_message(a);
-  client.send(out_msg).wait();
+string Message(string a)
+{
 
-  client.receive().then([](websocket_incoming_message in_msg) {
-    return in_msg.extract_string();
-  }).then([&response](string body) {
-response = body;
-  }).wait();
+    string response;
+    websocket_client client;
+    client.connect("ws://echo.websocket.org").wait();
 
-  client.close().wait();
-return "Responce: "+response;
-  }
+    websocket_outgoing_message out_msg;
 
-int main() {
-string a;
-getline(cin,a);
-cout << "Request: " << a  << endl;
-cout<< Message(a);
 
-  return 0;
+    out_msg.set_utf8_message(a);
+    client.send(out_msg).wait();
+
+    client.receive().then([](websocket_incoming_message in_msg)
+    {
+        return in_msg.extract_string();
+    }).then([&response](string body)
+    {
+        response = body;
+    }).wait();
+
+    client.close().wait();
+    _log << "Response: "+response << " ||| " + _datatime;
+
+    return "Response: "+response;
+}
+
+int main()
+{
+
+    mkdir("./logs", S_IRWXU | S_IRWXO | S_IRWXG);
+
+    string a;
+    getline(cin,a);
+    cout << "Request: " << a  << endl;
+    _log << "Request: " << a << " ||| " + _datatime;
+
+    cout << Message(a) << endl;
+
+
+    return 0;
 }
